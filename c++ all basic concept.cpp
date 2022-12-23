1296c6f4d17030f57cd1a15dd18195942c3a74f9
AJIT BISEN <115009722+ajitbisen123@users.noreply.github.com>
2022-12-23 19:49:54 +0530

    Add files via upload

---
+++ c++ all basic concept.cpp
@@ -0,0 +1,318 @@
+ #include<iostream>
+ using namespace std;
+ 
+  int main(){
+    hello world
+    //  cout<<"hello world";
+    //  return 0;
+    
+    
+    // char arr[]="welcome to c++ tutorial";
+    // cout<<"value of arr is"<<" " << arr <<endl;
+    // return 0;
+    
+    
+    input 
+    // int age;
+    // cout<<"enter your age :";
+    // cin>>age;
+    // cout<<"your age is"<<" "<<age;
+    // return 0;
+    
+    output
+    // cout<<"ajit"<<endl;
+    // cout<<"bisen"<<endl;
+    // cout<<"keshlewada"<<endl;
+    // return 0;
+    
+    variable
+    
+    // int a=5;
+    // char b='t';
+    // float c=4.5;
+    // double d=false;
+    // cout<< a<<endl;
+    // cout<< b<<endl;
+    // cout<< c<<endl;
+    // cout<< d;
+    // return 0;
+    
+    
+    
+    if/else
+    // int a=8;
+    // if(a%3==0){
+    //     cout<<"number is even";
+    // }
+    // else
+    // cout<<"number is odd";
+    // return 0;
+   
+   
+   
+   
+    
+    // int a;
+    // cout<<"enter a number ";
+    // cin>>a;
+    // if(a%2==0){
+    //     cout<<"a is even";
+    // }
+    // else{
+    //     cout<<"a is odd";
+    // }
+    // return 0;
+    
+
+if/else ladder
+//  int num;
+// cout<<"enter a number";
+// cin>>num;
+// if(num>=0&&num<=33){
+//     cout<<"you are fail";
+// }
+//  else if(num>=33&&num<=40){
+//     cout<<"you are pass in d grade";
+// }
+    
+// else if(num>=45&&num<60){
+//     cout<<"you are pass in c grade";
+// }
+// else if(num>=60&&num<75){
+//     cout<<"you are pass in b grade";
+// }
+//  else if(num>=75&&num<90){
+//     cout<<"you are pass in a grade";
+// }
+// else if(num>=90&&num<=100){
+//     cout<<"you are pass in a++ grade";
+// }
+
+// return 0;
+
+
+
+operator
+
+  // int a= 20;
+    // int b=10;
+    //  cout<<"a+b= "<<a+b<<endl;
+    //   cout<<"a-b= "<<a-b<<endl;
+    //   cout<<"a/b= "<<a/b<<endl;
+    //     cout<<"a%b= "<<a%b;
+    
+  
+//   int a=78;
+//   int b=40;
+//   cout<<a<<endl;
+//   cout<<b<<endl;
+//   a+=b;+
+
+//   cout<<a;
+  switch
+   
+//   int day;
+//   cin>>day;
+//   switch(day){
+//       case 1:
+//       cout<<"sunday";
+//       break;
+//       case 2:
+//       cout<<"monday";
+//       break;
+//       case 3:
+//       cout<<"tuesday";
+//       break;
+//       case 4:
+//       cout<<"wednesday";
+//       break;
+//       case 5:
+//       cout<<"thusday";
+//       break; 
+//       case 6:
+//       cout<<"friday";
+//       break;
+//       case 7:
+//       cout<<"saturday";
+//       break;
+//       default:
+//       cout<<"weakend";
+      
+      
+//   }
+
+
+for
+
+    //  int i;
+    //  for(i=1;i<=10;i++){
+    //      cout<<i<<endl;
+    //  }
+    
+//   int i;
+//   for(int i=1;i<=3;i++){
+//       for(int j=1;j<=3;j++){
+//           cout<<i<<" "<<j<<endl;
+//       }
+//   }
+    while
+
+// int i;
+// while(i<=20){
+//     cout<<i<<endl;
+//     i++;
+// }
+do while
+// int i;
+// do{
+//     cout<<i<<endl;
+//     i++;
+// }while(i<=10);
+
+
+continue and break;
+
+// int i;
+// for(i=1;i<=10;i++){
+//     if(i==8){
+//         // break;
+//         continue;
+//     }
+//     cout<<i<<endl;
+//    
+    
+ }
+    
+function
+
+
+// function declaration
+// void greed(){
+//     cout<<"hello world";
+// }
+
+// int main()
+// {
+//     //function calling
+// greed();
+
+
+
+
+
+
+//  void printnum(int num)
+// {
+//     cout<<num;
+// }
+
+// int main(){
+//     int n=5;
+//     printnum(5);
+
+ 
+//     return 0;
+ 
+// }
+
+
+
+
+
+//  void parameter(int x,float y){
+//     cout<<x<<endl;
+//     cout<<y;
+    
+// }
+//   int main()
+// {
+//     int a=5;
+//     float b=7.6;
+//     parameter(a,b);
+//     return 0;
+// }
+
+
+
+
+
+
+// int sun (int ,int);//function prototype
+
+//  int sum(int x,int y){
+//      cout<<(x+y) ;      //function declaration
+//      return 0;
+     
+// }
+
+// int main(){
+//     int a=5;
+//     int b=7;
+     
+//     sum(a,b);   //function calling
+     
+// }
+
+
+
+recursion
+
+
+// int factorial(int n){
+//     //base case
+//     if(n==0){
+//         return 1;
+//     }
+//     return n*factorial(n-1);
+// }
+
+// int main()
+// {
+//     int n;
+//     int ans;
+//     cin>>n;
+//     ans=factorial(n);
+//     cout<<ans;
+
+//     return 0;
+
+           pointer
+
+
+// int a=10;
+    // int *aptr;
+    // aptr=&a;
+    // cout<<*aptr<<endl;
+    // cout<<aptr<<endl;
+    // cout<<&a;
+    
+    
+
+
+    // int a=34;
+    // int *aptr=&a;
+    // cout<<aptr<<endl;
+    // aptr++;
+    // cout<<aptr;
+    
+    
+
+    // int arr[]={ 56,20,30};
+    // cout<<*arr<<endl;
+    // int *ptr=arr;
+    // for(int i=0;i<3;i++){
+    //     cout<<*ptr<<endl;
+    //     ptr++;
+    // }
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+ 
